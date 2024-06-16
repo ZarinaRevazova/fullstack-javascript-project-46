5a2196d34755c7b2bc3d9f5b130e8f2e860441de
Zarina Revazova <zarinarevazova8@gmail.com>
2024-06-16 22:36:55 +0300

    step 9 project

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 //fileURLToPath преобразует файловый URL в путь к файлу.
@@ -16,15 +16,15 @@ const getFixtureFilePath = (filename) => path.join(__dirname, '..', '__fixtures_
 
 const readFixtureFile = (filename) => fs.readFileSync(getFixtureFilePath(filename), 'utf-8');
 
-const basicFileFormat = ['json', 'yaml', 'yml'];
-const outputFileFormat = ['stylish', 'plain', 'json'];
+const inputFormat = ['json', 'yaml', 'yml'];
 
-test.each(basicFileFormat)('gendiff_test_format', (format) => {
-  const filepath1 = getFixtureFilePath(`file1.${format}`);
-  const filepath2 = getFixtureFilePath(`file2.${format}`);
-  outputFileFormat.forEach((outputFileFormat) => {
-    const expected = readFixtureFile(`${outputFileFormat}ExpectedString.txt`);
-    const diffResult = genDiff(filepath1, filepath2, outputFileFormat);
-    expect(diffResult).toBe(expected);
+describe('gendiff_testing', () => {
+  test.each(inputFormat)('gendiff_test_format', (format) => {
+    const filepath1 = getFixtureFilePath(`file1.${format}`);
+    const filepath2 = getFixtureFilePath(`file2.${format}`);
+    expect(genDiff(filepath1, filepath2)).toBe(readFixtureFile('stylishExpectedString.txt'));
+    expect(genDiff(filepath1, filepath2, 'stylish')).toBe(readFixtureFile('stylishExpectedString.txt'));
+    expect(genDiff(filepath1, filepath2, 'plain')).toBe(readFixtureFile('plainExpectedString.txt'));
+    expect(genDiff(filepath1, filepath2, 'json')).toBe(readFixtureFile('jsonExpectedString.txt'));
   });
 });
