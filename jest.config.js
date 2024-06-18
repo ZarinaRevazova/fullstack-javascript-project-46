d0f4afdc28dbbb515a635abc664ee12c1b81263b
Zarina Revazova <zarinarevazova8@gmail.com>
2024-06-18 13:09:45 +0300

    step 9 project_delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  coverageDirectory: 'coverage',
-  collectCoverage: false,
-  coverageProvider: 'v8',
-};
